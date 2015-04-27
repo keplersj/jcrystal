33a9078255f074886695fb053f4b32d0f69ba9d5
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-04-27 00:27:58 -0600

    This will allow us to create a JVM instance for a beckoning.

--- src/jcrystal.cr
+++ src/jcrystal.cr
@@ -9,3 +9,23 @@ private def object(package: String)
   #package.split(.)
   return JNI::JObject
 end
+
+def create_vm(jvm: JNI::JavaVM)
+    env:: JNI::JNIEnv
+    vm_args:: JavaVMInitArgs
+
+    options:: JNI::JavaVMOption
+    #Path to the java source code
+    #options.optionString = "-Djava.class.path=D:\\Java Src\\TestStruct" 
+    vm_args.version = JNI::JNI_VERSION_1_8 #JDK version. This indicates version 1.6
+    vm_args.nOptions = 1
+    vm_args.ignoreUnrecognized = 0
+
+    ret = JNI.create_java_vm(jvm, env, vm_args)
+
+    if(ret < 0)
+      puts "Unable to Launch JVM"
+    end
+
+    return env
+end
