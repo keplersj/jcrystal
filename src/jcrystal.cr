2800af39c50f806e62308c07ddc7fb67e698cd47
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-04-28 00:37:31 -0600

    Java Virtual Machine creation method placed in new object.

--- src/jcrystal.cr
+++ src/jcrystal.cr
@@ -10,22 +10,24 @@ private def object(package: String)
   return JNI::JObject
 end
 
-def create_vm(jvm: JNI::JavaVM)
-    env:: JNI::JNIEnv
-    vm_args:: JavaVMInitArgs
+module JCrystal
+  def self.create_vm(jvm: JNI::JavaVM)
+      env:: JNI::JNIEnv
+      vm_args:: JNI::JavaVMInitArgs
 
-    options:: JNI::JavaVMOption
-    #Path to the java source code
-    #options.optionString = "-Djava.class.path=D:\\Java Src\\TestStruct" 
-    vm_args.version = JNI::JNI_VERSION_1_8 #JDK version. This indicates version 1.6
-    vm_args.nOptions = 1
-    vm_args.ignoreUnrecognized = 0
+      options:: JNI::JavaVMOption
+      #Path to the java source code
+      #options.optionString = "-Djava.class.path=D:\\Java Src\\TestStruct"
+      vm_args.version = JNI::JNI_VERSION_1_8 #JDK version. This indicates version 1.6
+      vm_args.nOptions = 1
+      vm_args.ignoreUnrecognized = 0
 
-    ret = JNI.create_java_vm(jvm, env, vm_args)
+      ret = JNI.create_java_vm(jvm, env, vm_args)
 
-    if(ret < 0)
-      puts "Unable to Launch JVM"
-    end
+      if(ret < 0)
+        puts "Unable to Launch JVM"
+      end
 
-    return env
+      return env
+  end
 end
