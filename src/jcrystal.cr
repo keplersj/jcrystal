23bde623c2a90c551a2131275468da689228e58c
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-05-05 18:03:18 -0600

    Remove comments from macro.

--- src/jcrystal.cr
+++ src/jcrystal.cr
@@ -6,13 +6,7 @@ macro jimport(object)
 end
 
 macro init_crystal
-  # We need to initialize the GC
   GC.init
-
-  # We need to invoke Crystal's "main" function, the one that initializes
-  # all constants and runs the top-level code (none in this case, but without
-  # constants like STDOUT and others the last line will crash).
-  # We pass 0 and null to argc and argv.
   LibCrystalMain.__crystal_main(0, Pointer(Pointer(UInt8)).null)
 end
 
